cf31217eae71c0f230ae620a9894a44ce4b808fa
Luiz Serra <lserra.trade@gmail.com>
2020-07-14 20:57:52 -0300

    estilizando a pagina de detalhe do repositório

--- git-repo-api/src/pages/Repository/index.tsx
+++ git-repo-api/src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './style';
 
 interface RepositoryParams {
     repository: string;
@@ -7,6 +12,47 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/"> <FiChevronLeft size={16} /> Voltar</Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars0.githubusercontent.com/u/31904963?s=460&u=dddc6c1b0dfbf9d6d8d29346e7b9ded89b48f71e&v=4" alt="avatar" />
+          <div>
+            <strong>{params.repository}</strong>
+            <p>Um repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>78</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="/">
+
+          <div>
+            <strong>ABC</strong>
+            <p>DEF</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+
+    </>
+  );
 };
 export default Repository;
